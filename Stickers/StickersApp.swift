a027a944b91d6d56fad9eddbcbd1f8bcdf394367
Liran <liranbd1@gmail.com>
2022-12-30 15:40:10 +0200

    Initial Commit

---
+++ Stickers/StickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickersApp.swift
+//  Stickers
+//
+//  Created by Liran on 30/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct StickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
